8f03de5ded30fd0b5a9d447461aee323116d17bf
Nikita Lagutkin <nikitalagut03@gmail.com>
2024-02-06 15:27:19 +0300

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,23 @@
 package org.example;
 
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
-        String folderPath = "C:/Users/nikit/OneDrive/Рабочий стол/NIASPO";
+
+        String folderPath = "C:/Users/nikit/OneDrive/Рабочий стол";
         File file = new File(folderPath);
-        System.out.println(getFolderSize(file));
+        long start = System.currentTimeMillis();
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+
+//        System.out.println(getFolderSize(file));
+
+        long duration = (System.currentTimeMillis() - start) / 1000;
+        System.out.println(duration + " seconds");
     }
 
     public static long getFolderSize(File folder) {
